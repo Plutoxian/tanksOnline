ef455c56ea4d6d5d903ed4b186f4ace2403dc593
Preston Mills <Plutoxian@users.noreply.github.com>
2016-05-30 13:28:23 -0500

    testing drawing

--- tanks/javascript/game.js
+++ tanks/javascript/game.js
@@ -1,9 +1,8 @@
-var draw;
+var draw = document.getElementById("myCanvas").getContext("2d");
 
-function onLoad(){
-  draw = document.getElementById("myCanvas").getContext("2d");
-}
-
+draw.fillStyle = "red";
+draw.fillRect(50, 50, 200, 200);
+  
 function fire(){
   draw.fillStyle = "blue";
   draw.fillRect(50, 50, 200, 200);
