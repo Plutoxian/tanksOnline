75ddcb29e091c1eb0727d4acff6b39fb2bb6ddb1
Preston Mills <Plutoxian@users.noreply.github.com>
2016-05-30 18:09:17 -0500

    the ground now exists

--- tanks/javascript/game.js
+++ tanks/javascript/game.js
@@ -2,11 +2,31 @@ var draw;
 var tankArray;
 var currentTurn;
 
+var ground;
+
 var moveLeft;
 var moveRight;
 
+function drawGround() {
+    var i;
+    draw.fillStyle = "#228B22";
+    draw.moveTo(0, 150 - ground[0]);
+    for (i = 1; i < 51; i += 1) {
+        draw.lineTo(i * 6, 150 - ground[i]);
+    }
+    draw.lineTo(300, 150);
+    draw.lineTo(0, 150);
+    draw.fill();
+}
+
+function colDet(tankNum) {
+    tankArray[tankNum].y = (150 - ground[(((tankArray[tankNum].x) + 5) - (((tankArray[tankNum].x) + 5) % 6)) / 6]) - ((ground[1 + ((((tankArray[tankNum].x) + 5) - (((tankArray[tankNum].x) + 5) % 6)) / 6)] - ground[(((tankArray[tankNum].x) + 5) - (((tankArray[tankNum].x) + 5) % 6)) / 6]) * (((tankArray[tankNum].x) + 5) % 6) / 5) - 5;
+    tankArray[tankNum].angle = Math.atan((ground[1 + ((((tankArray[tankNum].x) + 5) - (((tankArray[tankNum].x) + 5) % 6)) / 6)] - ground[(((tankArray[tankNum].x) + 5) - (((tankArray[tankNum].x) + 5) % 6)) / 6]) / 6);
+}
+
 function mainLoop() {
-    draw.fillStyle = "#FFFFFF";
+    draw.rotate(0);
+    draw.fillStyle = "#C0EDFF";
     draw.fillRect(0, 0, document.getElementById("myCanvas").width, document.getElementById("myCanvas").height);
     
     if (moveRight) {
@@ -17,8 +37,18 @@ function mainLoop() {
     }
     if (moveLeft) {
         tankArray[currentTurn].x -= 1;
+        if (tankArray[currentTurn].x < 0) {
+            tankArray[currentTurn].x = 0;
+        }
     }
-
+    
+    var i;
+    for (i = 0; i < 6; i += 1) {
+        colDet(i);
+    }
+    
+    drawGround();
+    
     draw.fillStyle = "#FF0000";
     draw.fillRect(tankArray[0].x, tankArray[0].y, 10, 5);
     
@@ -36,20 +66,26 @@ function mainLoop() {
     
     draw.fillStyle = "#FF7F00";
     draw.fillRect(tankArray[5].x, tankArray[5].y, 10, 5);
-
+    
     setTimeout(mainLoop, 30);
 }
 
 function init() {
     //do initiation stuff here
-    tankArray = [{exists: 1, x: 0, y: 50, power: 100, angle: 90},
-                 {exists: 1, x: 50, y: 50, power: 100, angle: 90},
-                 {exists: 1, x: 100, y: 50, power: 100, angle: 90},
-                 {exists: 1, x: 150, y: 50, power: 100, angle: 90},
-                 {exists: 1, x: 200, y: 50, power: 100, angle: 90},
-                 {exists: 1, x: 250, y: 50, power: 100, angle: 90}];
+    tankArray = [{exists: 1, x: 0, y: 50, power: 100, angle: 0},
+                 {exists: 1, x: 50, y: 50, power: 100, angle: 0},
+                 {exists: 1, x: 100, y: 50, power: 100, angle: 0},
+                 {exists: 1, x: 150, y: 50, power: 100, angle: 0},
+                 {exists: 1, x: 200, y: 50, power: 100, angle: 0},
+                 {exists: 1, x: 250, y: 50, power: 100, angle: 0}];
     
     currentTurn = 0;
+    
+    ground = [50,50,50,50,50,50,50,50,50,50,
+              60,60,60,60,60,60,60,60,60,60,
+              50,50,50,50,51,53,56,58,59,58,
+              56,53,51,50,50,50,50,50,50,50,
+              49,47,44,40,35,29,22,14,10,10,10];
     
     mainLoop();
 }
