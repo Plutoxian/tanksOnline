5c6f6e27753c7a0d7b1bd12a84f88c7f6bba87e2
Preston Mills <Plutoxian@users.noreply.github.com>
2016-05-30 16:40:34 -0500

    no more strict mode

--- tanks/javascript/game.js
+++ tanks/javascript/game.js
@@ -1,61 +1,87 @@
 var draw;
-var tank;
+var tankArray;
+var currentTurn;
 
 var moveLeft;
 var moveRight;
 
 function mainLoop() {
-    "use strict";
     draw.fillStyle = "#FFFFFF";
     draw.fillRect(0, 0, document.getElementById("myCanvas").width, document.getElementById("myCanvas").height);
     
-    if (moveLeft) {
-        tank -= 1;
-    }
     if (moveRight) {
-        tank += 1;
+        tankArray[currentTurn].x += 1;
+        if (tankArray[currentTurn].x > (document.getElementById("myCanvas").width - 10)) {
+            tankArray[currentTurn].x = (document.getElementById("myCanvas").width - 10);
+        }
     }
+    if (moveLeft) {
+        tankArray[currentTurn].x -= 1;
+    }
+
+    draw.fillStyle = "#FF0000";
+    draw.fillRect(tankArray[0].x, tankArray[0].y, 10, 5);
     
-    draw.fillStyle = "#000000";
-    draw.fillRect(tank, 50, 50, 50);
+    draw.fillStyle = "#007FFF";
+    draw.fillRect(tankArray[1].x, tankArray[1].y, 10, 5);
+    
+    draw.fillStyle = "#00FF00";
+    draw.fillRect(tankArray[2].x, tankArray[2].y, 10, 5);
+    
+    draw.fillStyle = "#7F00FF";
+    draw.fillRect(tankArray[3].x, tankArray[3].y, 10, 5);
+    
+    draw.fillStyle = "#FFFF00";
+    draw.fillRect(tankArray[4].x, tankArray[4].y, 10, 5);
+    
+    draw.fillStyle = "#FF7F00";
+    draw.fillRect(tankArray[5].x, tankArray[5].y, 10, 5);
+
     setTimeout(mainLoop, 30);
 }
 
 function init() {
-    "use strict";
     //do initiation stuff here
-    tank = 0;
+    tankArray = [{exists: 1, x: 0, y: 50, power: 100, angle: 90},
+                 {exists: 1, x: 50, y: 50, power: 100, angle: 90},
+                 {exists: 1, x: 100, y: 50, power: 100, angle: 90},
+                 {exists: 1, x: 150, y: 50, power: 100, angle: 90},
+                 {exists: 1, x: 200, y: 50, power: 100, angle: 90},
+                 {exists: 1, x: 250, y: 50, power: 100, angle: 90}];
+    
+    currentTurn = 0;
     
     mainLoop();
 }
 
 function onLoad() {
-    "use strict";
     draw = document.getElementById("myCanvas").getContext("2d");
     init();
 }
 
 function onMoveLeftDown() {
-    "use strict";
     moveLeft = true;
+    moveRight = false;
 }
 
 function onMoveLeftUp() {
-    "use strict";
     moveLeft = false;
 }
 
 function onMoveRightDown() {
-    "use strict";
     moveRight = true;
+    moveLeft = false;
 }
 
 function onMoveRightUp() {
-    "use strict";
     moveRight = false;
 }
 
 function onFire() {
-    "use strict";
-    //to do
+    moveLeft = false;
+    moveRight = false;
+    currentTurn += 1;
+    if (currentTurn === 6) {
+        currentTurn = 0;
+    }
 }
